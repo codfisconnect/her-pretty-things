6596be0f9ae6bda4a63f20488c80863fcb9b12c8
codfisconnect <codfis.connect@gmail.com>
2026-09-13 15:25:06 +0530

    Fix unused icon import

--- src/pages/Scoops/Scoops.tsx
+++ src/pages/Scoops/Scoops.tsx
@@ -1,4 +1,4 @@
-import { Gift, Heart, Sparkles } from "lucide-react";
+import { Sparkles } from "lucide-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { addCartItem } from "../../services/cartService";
